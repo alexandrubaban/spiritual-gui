8f270f0ff31a7e108e09826bb74f8f28318080f8
Daniel Spitzer <dsp@tradeshift.com>
2013-08-28 13:22:56 +0200

    recompiled dist with new stuff

--- dist/spiritual-gui-0.0.5.js
+++ dist/spiritual-gui-0.0.5.js
@@ -6612,27 +6612,9 @@ Object.defineProperties ( gui.BoxPlugin.prototype, {
  * @using gui.Arguments#confirmed
  * @using gui.Combo#chained
  */
-( function using ( confirmed, chained ) {
+gui.Broadcast = ( function using ( confirmed, chained ) {
 
-	/**
-	 * Broadcast constructor.
-	 * @TODO "one" and "oneGlobal" methods...
-	 * @param {Spirit} target
-	 * @param {String} type
-	 * @param {object} data
-	 * @param {boolean} global
-	 */
-	gui.Broadcast = function ( target, type, data, global, sig, contextids ) {
-
-		this.target = target;
-		this.type = type;
-		this.data = data;
-		this.isGlobal = global;
-		this.$contextid = sig || gui.$contextid;
-		this.$contextids = contextids || [];
-	};
-
-	gui.Broadcast.prototype = {
+	return gui.Class.create ( Object.prototype, {
 
 		/**
 		 * Broadcast target.
@@ -6672,236 +6654,255 @@ Object.defineProperties ( gui.BoxPlugin.prototype, {
 		 * @type {Array<String>}
 		 */
 		$contextids : null,
-		
+	
 		/**
-		 * Identification
+		 * Constructor.
+		 * @param {Map<String,object>} defs
+		 */		
+		$onconstruct : function ( defs ) {
+			gui.Object.extend ( this, defs );
+			this.$contextids = this.$contextids || [];
+		}
+
+
+	}, {}, { // Static ................................................................................
+
+		/**
+		 * Tracking global handlers (mapping broadcast types to list of handlers).
+		 * @type {Map<String,<Array<object>>}
+		 */
+		_globals : Object.create ( null ),
+
+		/**
+		 * Tracking local handlers (mapping gui.$contextids to broadcast types to list of handlers).
+		 * @type {Map<String,Map<String,Array<object>>>}
+		 */
+		_locals : Object.create ( null ),
+
+		/**
+		 * mapcribe handler to message.
+		 * @param {object} message String or array of strings
+		 * @param {object} handler Implements BroadcastListener
+		 * @param @optional {String} sig
+		 * @returns {function}
+		 */
+		add : chained ( function ( message, handler, sig ) {
+			this._add ( message, handler, sig || gui.$contextid );
+		}),
+
+		/**
+		 * Unmapcribe handler from broadcast.
+		 * @param {object} message String or array of strings
+		 * @param {object} handler
+		 * @param @optional {String} sig
+		 * @returns {function}
+		 */
+		remove : chained ( function ( message, handler, sig ) {
+			this._remove ( message, handler, sig || gui.$contextid );
+		}),
+
+		/**
+		 * Publish broadcast in local window scope.
+		 * @TODO queue for incoming dispatch (finish current message first).
+		 * @param {Spirit} target
+		 * @param {String} type
+		 * @param {object} data
+		 * @param {String} sig
+		 * @returns {gui.Broadcast}
+		 */
+		dispatch : function ( target, type, data, sig ) {
+			return this.$dispatch ({
+				target : target,
+				type : type,
+				data : data,
+				isGlobal : false,
+				$contextid : sig || gui.$contextid
+			});
+		},
+
+		/**
+		 * mapcribe handler to message globally.
+		 * @param {object} message String or array of strings
+		 * @param {object} handler Implements BroadcastListener
+		 * @returns {function}
+		 */
+		addGlobal : chained ( function ( message, handler ) {
+			this._add ( message, handler );
+		}),
+
+		/**
+		 * Unmapcribe handler from global broadcast.
+		 * @param {object} message String or array of strings
+		 * @param {object} handler
+		 * @returns {function}
+		 */
+		removeGlobal : chained ( function ( message, handler ) {
+			this._remove ( message, handler );
+		}),
+
+		/**
+		 * Dispatch broadcast in global scope (all windows).
+		 * @TODO queue for incoming dispatch (finish current first).
+		 * @TODO Handle remote domain iframes ;)
+		 * @param {Spirit} target
+		 * @param {String} type
+		 * @param {object} data
+		 * @returns {gui.Broadcast}
+		 */
+		dispatchGlobal : function ( target, type, data ) {
+			return this.$dispatch ({
+				target : target,
+				type : type,
+				data : data,
+				isGlobal : true,
+				$contextid : gui.$contextid
+			});
+		},
+
+		/**
+		 * Encode broadcast to be posted xdomain.
+		 * @param {gui.Broacast} b
 		 * @returns {String}
 		 */
-		toString : function () {	
-			return "[object gui.Broadcast]";
-		}
-	};
+		stringify : function ( b ) {
+			var prefix = "spiritual-broadcast:";
+			return prefix + ( function () {
+				b.target = null;
+				b.data = ( function ( d ) {
+					if ( gui.Type.isComplex ( d )) {
+						if ( gui.Type.isFunction ( d.stringify )) {
+							d = d.stringify ();
+						} else {
+							try {
+								JSON.stringify ( d ); // @TODO: think mcfly - how come not d = JSON.stringify????
+							} catch ( jsonexception ) {
+								d = null;
+							}
+						}
+					}
+					return d;
+				}( b.data ));
+				return JSON.stringify ( b );
+			}());
+		},
 
-	
-	// Static ................................................................................
-	
-	/**
-	 * Tracking global handlers (mapping broadcast types to list of handlers).
-	 * @type {Map<String,<Array<object>>}
-	 */
-	gui.Broadcast._globals = Object.create ( null );
+		/**
+		 * Decode broadcast posted from xdomain and return a broadcast-like object.
+		 * @param {String} msg
+		 * @returns {object}
+		 */
+		parse : function ( msg ) {
+			var prefix = "spiritual-broadcast:";
+			if ( msg.startsWith ( prefix )) {
+				return JSON.parse ( msg.split ( prefix )[ 1 ]);
+			}
+		},
 
-	/**
-	 * Tracking local handlers (mapping gui.$contextids to broadcast types to list of handlers).
-	 * @type {Map<String,Map<String,Array<object>>>}
-	 */
-	gui.Broadcast._locals = Object.create ( null );
 
-	/**
-	 * mapcribe handler to message.
-	 * @param {object} message String or array of strings
-	 * @param {object} handler Implements BroadcastListener
-	 * @param @optional {String} sig
-	 * @returns {function}
-	 */
-	gui.Broadcast.add = chained ( function ( message, handler, sig ) {
-		this._add ( message, handler, sig || gui.$contextid );
-	});
+		// PRIVATE ...................................................................................
 
-	/**
-	 * Unmapcribe handler from broadcast.
-	 * @param {object} message String or array of strings
-	 * @param {object} handler
-	 * @param @optional {String} sig
-	 * @returns {function}
-	 */
-	gui.Broadcast.remove = chained ( function ( message, handler, sig ) {
-		this._remove ( message, handler, sig || gui.$contextid );
-	});
-
-	/**
-	 * Publish broadcast in local window scope.
-	 * @TODO queue for incoming dispatch (finish current message first).
-	 * @param {Spirit} target
-	 * @param {String} type
-	 * @param {object} data
-	 * @param {String} sig
-	 * @returns {gui.Broadcast}
-	 */
-	gui.Broadcast.dispatch = function ( target, type, data, sig ) {
-		return this._dispatch ( target, type, data, sig || gui.$contextid );
-	};
-
-	/**
-	 * mapcribe handler to message globally.
-	 * @param {object} message String or array of strings
-	 * @param {object} handler Implements BroadcastListener
-	 * @returns {function}
-	 */
-	gui.Broadcast.addGlobal = chained ( function ( message, handler ) {
-		this._add ( message, handler );
-	});
-
-	/**
-	 * Unmapcribe handler from global broadcast.
-	 * @param {object} message String or array of strings
-	 * @param {object} handler
-	 * @returns {function}
-	 */
-	gui.Broadcast.removeGlobal = chained ( function ( message, handler ) {
-		this._remove ( message, handler );
-	});
-
-	/**
-	 * Dispatch broadcast in global scope (all windows).
-	 * @TODO queue for incoming dispatch (finish current first).
-	 * @TODO Handle remote domain iframes ;)
-	 * @param {Spirit} target
-	 * @param {String} type
-	 * @param {object} data
-	 * @returns {gui.Broadcast}
-	 */
-	gui.Broadcast.dispatchGlobal = function ( target, type, data, contextids ) {
-		return this._dispatch ( target, type, data, null, contextids );
-	};
-
-	/**
-	 * Encode broadcast to be posted xdomain.
-	 * @param {gui.Broacast} b
-	 * @returns {String}
-	 */
-	gui.Broadcast.stringify = function ( b ) {
-		var prefix = "spiritual-broadcast:";
-		return prefix + ( function () {
-			b.target = null;
-			b.data = ( function ( d ) {
-				if ( gui.Type.isComplex ( d )) {
-					if ( gui.Type.isFunction ( d.stringify )) {
-						d = d.stringify ();
+		/**
+		 * Subscribe handler to message(s).
+		 * @param {Array<string>|string} type
+		 * @param {object|function} handler Implements BroadcastListener
+		 * @param @optional {String} sig
+		 */
+		_add : confirmed ( "array|string", "object|function", "(string)" ) (
+			function ( type, handler, sig ) {
+				if ( gui.Interface.validate ( gui.IBroadcastHandler, handler )) {
+					if ( gui.Type.isArray ( type )) {
+						type.forEach ( function ( t ) {
+							this._add ( t, handler, sig );
+						}, this );
 					} else {
-						try {
-							JSON.stringify ( d ); // @TODO: think mcfly - how come not d = JSON.stringify????
-						} catch ( jsonexception ) {
-							d = null;
+						var map;
+						if ( sig ) {
+							map = this._locals [ sig ];
+							if ( !map ) {
+								map = this._locals [ sig ] = Object.create ( null ); 
+							}
+						} else {
+							map = this._globals;
+						}
+						if ( !map [ type ]) {
+							map [ type ] = [];
+						}
+						var array = map [ type ];
+						if ( array.indexOf ( handler ) === -1 ) {
+							array.push ( handler );
 						}
 					}
 				}
-				return d;
-			}( b.data ));
-			return JSON.stringify ( b );
-		}());
-	};
+			}
+		),
 
-	/**
-	 * Decode broadcast posted from xdomain and return a broadcast-like object.
-	 * @param {String} msg
-	 * @returns {object}
-	 */
-	gui.Broadcast.parse = function ( msg ) {
-		var prefix = "spiritual-broadcast:";
-		if ( msg.startsWith ( prefix )) {
-			return JSON.parse ( msg.split ( prefix )[ 1 ]);
-		}
-	};
-
-
-	// PRIVATE ...................................................................................
-
-	/**
-	 * Subscribe handler to message(s).
-	 * @param {Array<string>|string} type
-	 * @param {object|function} handler Implements BroadcastListener
-	 * @param @optional {String} sig
-	 */
-	gui.Broadcast._add = confirmed ( "array|string", "object|function", "(string)" ) (
-		function ( type, handler, sig ) {
+		/**
+		 * Hello.
+		 * @param {object} message String or array of strings
+		 * @param {object} handler
+		 * @param @optional {String} sig
+		 */
+		_remove : function ( message, handler, sig ) {
 			if ( gui.Interface.validate ( gui.IBroadcastHandler, handler )) {
-				if ( gui.Type.isArray ( type )) {
-					type.forEach ( function ( t ) {
-						this._add ( t, handler, sig );
+				if ( gui.Type.isArray ( message )) {
+					message.forEach ( function ( msg ) {
+						this._remove ( msg, handler, sig );
 					}, this );
 				} else {
-					var map;
-					if ( sig ) {
-						map = this._locals [ sig ];
-						if ( !map ) {
-							map = this._locals [ sig ] = Object.create ( null ); 
+					var index, array = ( function ( locals, globals) {
+						if ( sig ) {
+							if ( locals [ sig ]) {
+								return locals [ sig ][ message ];
+							}
+						} else {
+							return globals [ message ];
 						}
-					} else {
-						map = this._globals;
-					}
-					if ( !map [ type ]) {
-						map [ type ] = [];
-					}
-					var array = map [ type ];
-					if ( array.indexOf ( handler ) === -1 ) {
-						array.push ( handler );
+					}( this._locals, this._globals ));
+					if ( array ) {
+						index = array.indexOf ( handler );
+						if ( index > -1 ) {
+							gui.Array.remove ( array, index );
+						}
 					}
 				}
 			}
-		}
-	);
+		},
 
-	/**
-	 * Hello.
-	 * @param {object} message String or array of strings
-	 * @param {object} handler
-	 * @param @optional {String} sig
-	 */
-	gui.Broadcast._remove = function ( message, handler, sig ) {
-		if ( gui.Interface.validate ( gui.IBroadcastHandler, handler )) {
-			if ( gui.Type.isArray ( message )) {
-				message.forEach ( function ( msg ) {
-					this._remove ( msg, handler, sig );
-				}, this );
-			} else {
-				var index, array = ( function ( locals, globals) {
-					if ( sig ) {
-						if ( locals [ sig ]) {
-							return locals [ sig ][ message ];
-						}
-					} else {
-						return globals [ message ];
-					}
-				}( this._locals, this._globals ));
-				if ( array ) {
-					index = array.indexOf ( handler );
-					if ( index > -1 ) {
-						gui.Array.remove ( array, index );
-					}
+		/**
+		 * Dispatch broadcast.
+		 * @param {Map<String,object>} b
+		 */
+		$dispatch : function ( b ) {
+		 /*
+		 * @param {Spirit} target
+		 * @param {String} type
+		 * @param {object} data
+		 * @param @optional {String} sig
+		 * @returns {gui.Broadcast}
+		 */
+			var map = b.isGlobal ? this._globals : this._locals [ b.$contextid ];
+			if ( b instanceof gui.Broadcast === false ) {
+				b = new gui.Broadcast ( b );	
+			}
+			if ( map ) {
+				var handlers = map [ b.type ];
+				if ( handlers ) {
+					handlers.slice ().forEach ( function ( handler ) {
+						handler.onbroadcast ( b );
+					});
 				}
 			}
+			if ( b.isGlobal ) {
+				var root = document.documentElement.spirit;
+				if ( root ) { // no spirit before DOMContentLoaded
+					root.propagateBroadcast ( b );
+				}
+			}
+			return b;
 		}
-	};
 
-	/**
-	 * Hello.
-	 * @param {Spirit} target
-	 * @param {String} type
-	 * @param {object} data
-	 * @param @optional {String} sig
-	 * @returns {gui.Broadcast}
-	 */
-	gui.Broadcast._dispatch = function ( target, type, data, sig, contextids ) {
-		var global = !gui.Type.isString ( sig );
-		var map = global ? this._globals : this._locals [ sig ];
-		var b = new gui.Broadcast ( target, type, data, global, sig, contextids );
-		if ( map ) {
-			var handlers = map [ type ];
-			if ( handlers ) {
-				handlers.slice ().forEach ( function ( handler ) {
-					handler.onbroadcast ( b );
-				});
-			}
-		}
-		if ( global ) {
-			var root = document.documentElement.spirit;
-			if ( root ) { // no spirit before DOMContentLoaded
-				root.propagateBroadcast ( b );
-			}
-		}
-		return b;
-	};
+	});
+	
 
 }( gui.Arguments.confirmed, gui.Combo.chained ));
 
@@ -10501,20 +10502,14 @@ gui.DocumentSpirit = gui.Spirit.extend ({
 	 * 
 	 * 1. Relay broadcasts
 	 * 2. Relay descending actions
-	 * @TODO Don't claim this as action target!
 	 * @param {String} msg
 	 */
 	_onmessage : function ( msg ) {
 		var pattern = "spiritual-broadcast";
 		if ( msg.startsWith ( pattern )) {
 			var b = gui.Broadcast.parse ( msg );
-			if ( b.$contextids.indexOf ( this.window.gui.$contextid ) < 0 ) {
-				gui.Broadcast.dispatchGlobal ( 
-					b.target, 
-					b.type, 
-					b.data,
-					b.$contextids
-				);
+			if ( this._relaybroadcast ( b.$contextids )) {
+				gui.Broadcast.$dispatch ( b );
 			}
 		} else {
 			pattern = "spiritual-action";
@@ -10531,6 +10526,17 @@ gui.DocumentSpirit = gui.Spirit.extend ({
 				}
 			}
 		}
+	},
+
+	/**
+	 * Should relay broadcast that has been postmessaged somwhat over-aggresively?
+	 * @param {Array<String>} ids
+	 * @returns {boolean}
+	 */
+	_relaybroadcast : function ( ids ) {
+		return [ gui.$contextid, this.window.gui.$contextid ].every ( function ( id ) {
+			return ids.indexOf ( id ) < 0;
+		});
 	},
 
 	/**
@@ -12847,6 +12853,7 @@ gui.Guide = {
 	_step1 : function ( doc ) {
 		var win = doc.defaultView;
 		var sig = win.gui.$contextid;
+		gui.Broadcast.removeGlobal (gui.BROADCAST_KICKSTART, this); //we don't need to listen anymore
 		this._metatags ( win ); // configure runtime
 		win.gui.start (); // channel spirits
 		this._stylesheets ( win ); // more spirits?
